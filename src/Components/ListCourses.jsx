59ece4eca92564031d5ed97dad0b64e857e99a34
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-02-17 01:26:37 +0100

    list course section styling

--- src/Components/ListCourses.jsx
+++ src/Components/ListCourses.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import rec55 from '../assets/Rectangle 55.png'
+import courseList from '../courseList'
 
 const ListCourses = () => {
   return (
-    <div>
-        <img src="" alt="" />
-        <div>
-            <p className='text-2xl text-btnBlue'>List Courses</p>
-            <p></p>
+    <div className='py-6 px-4 flex flex-col gap-4'>
+        <p className='text-2xl text-btnBlue '>List Courses</p>
+
+        <div className='flex gap-3 items-center'>
+            <img src={rec55} alt="" className='bg-white' />
+            <div className='flex flex-col gap-1'>
+                <p className=' text-base font-semibold'>Slider Application</p>
+                <p className='text-gray-600 text-sm  w-2/3'>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
         </div>
+        
         
 
 
