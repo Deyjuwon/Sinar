9304e19580960578e4818fa5aed4ecaac0963306
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-02-18 23:22:47 +0100

    list course section styling

--- src/Components/ListCourses.jsx
+++ src/Components/ListCourses.jsx
@@ -7,8 +7,9 @@ const ListCourses = () => {
     <div className='py-6 px-4 flex flex-col gap-4'>
         <p className='text-2xl text-btnBlue '>List Courses</p>
 
+        
         <div className='flex gap-3 items-center'>
-            <img src={rec55} alt="" className='bg-white' />
+           <img src={rec55} alt="" className='bg-white' />
             <div className='flex flex-col gap-1'>
                 <p className=' text-base font-semibold'>Slider Application</p>
                 <p className='text-gray-600 text-sm  w-2/3'>Lorem ipsum dolor sit amet consectetur.</p>
