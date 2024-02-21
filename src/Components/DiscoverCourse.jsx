832a2ca01373b1628399066a099838cc0f8dd1c1
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-02-21 14:30:12 +0100

    discover course section

---
+++ src/Components/DiscoverCourse.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DiscoverCourse = () => {
+  return (
+    <div className='bg-white w-full h-80 border border-red-500 mt-10'>
+
+
+    </div>
+  )
+}
+
+export default DiscoverCourse
--- src/Pages/DiscoverPage.jsx
+++ src/Pages/DiscoverPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
 import DiscoverHero from '../Components/DiscoverHero'
+import DiscoverCourse from '../Components/DiscoverCourse'
 
 const DiscoverPage = () => {
   return (
     <div>
         <Header />
         <DiscoverHero />
+        <DiscoverCourse />
     </div>
   )
 }
