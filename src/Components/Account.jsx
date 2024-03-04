d5048d28081003157bad0c7f834888908704c21c
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-03-04 22:19:24 +0100

    styling account page

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -4,13 +4,16 @@ import notifications from '../assets/Notification.png'
 import messages from '../assets/messages.png'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
+import genProfile from '../assets/genProfile.png'
+import Button from './Button';
+
 
 const Account = () => {
     const [isOpened, setIsOpened] = useState(false)
     
 
     return (
-        <div>
+        <div className='px-4'>
             
             <header className='w-full  h-32  flex items-center justify-between p-4'>
 
@@ -33,6 +36,27 @@ const Account = () => {
                 
                 
         </header>
+        <div className='flex flex-col gap-2 items-center'>
+            <img src={genProfile} alt="" />
+            <h1 className=' text-2xl text-blue-txt font-semibold'>Public profile</h1>
+            <p className='text-blue-txt text-sm text-center'>Add Information about yourself</p>
+        </div>
+        <div>
+            <form action="" className='flex flex-col '>
+                <label className='text-blue-txt text-sm pb-1' htmlFor="">Name</label>
+                <input className='w-full bg-white border border-gray-300 h-14 rounded-xl mb-4 outline-none pl-3' type="text" placeholder='Enter email...' />
+                <label className='text-blue-txt text-sm pb-1' htmlFor="">Country</label>
+                <input className='w-full bg-white border border-gray-300 h-14 rounded-xl mb-8 outline-none pl-3' type="text" placeholder='Enter password...' />
+                <label className='text-blue-txt text-sm pb-1' htmlFor="">Email Address</label>
+                <input className='w-full bg-white border border-gray-300 h-14 rounded-xl mb-4 outline-none pl-3' type="email" placeholder='Enter email...' />
+                <label className='text-blue-txt text-sm pb-1' htmlFor="">New password</label>
+                <input className='w-full bg-white border border-gray-300 h-14 rounded-xl mb-4 outline-none pl-3' type="password" placeholder='Enter email...' />
+                <label className='text-blue-txt text-sm pb-1' htmlFor="">New password repeat</label>
+                <input className='w-full bg-white border border-gray-300 h-14 rounded-xl mb-4 outline-none pl-3' type="password" placeholder='Enter email...' />
+                <Button name='Change password' />
+            </form>
+        </div>
+
         </div>
     )
     }
