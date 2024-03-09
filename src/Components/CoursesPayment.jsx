444132b938deb19b6dceabb34dca648345a79bd2
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-03-09 23:55:10 +0100

    styling account page

--- src/Components/CoursesPayment.jsx
+++ src/Components/CoursesPayment.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import NotificationHeader from './NotificationHeader'
 import backIcon from '../assets/Frame 45.png'
+import ListCourses from './ListCourses'
 
 const CoursesPayment = () => {
   return (
     <div>
         <NotificationHeader />
-        <div className='flex gap-4 items-center p-4'>
+        <div className='flex gap-4 items-center p-4 '>
             <img src={backIcon} alt="" className='' />
             <p className='text-base font-medium'>Turn Back</p>
+        
         </div>
+        <div>
+              <ListCourses />
+            </div>
     </div>
   )
 }
--- src/Pages/PaymentPanelPage.jsx
+++ src/Pages/PaymentPanelPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CoursesPayment from '../Components/CoursesPayment'
+import ListCourses from '../Components/ListCourses'
 
 const PaymentPanelPage = () => {
   return (
     <div>
         <CoursesPayment />
+        
     </div>
   )
 }
