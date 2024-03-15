59de7cd82cd9084a8f64f85e153da33e11dadc69
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-03-15 16:30:50 +0100

    home page responsiveness

--- src/Components/Acheivement.jsx
+++ src/Components/Acheivement.jsx
@@ -3,33 +3,38 @@ import sugarCube from '../assets/sugar-cube 3.png';
 
 const Acheivement = () => {
   return (
-    <div className='bg-white mt-20 w-full h-362 gap-6 flex flex-col items-center pt-8'>
-        <p className='font-semibold text-2xl'>Everything is Achievement</p>
-        <p className='text-gray-400 text-xs w-5/6 text-center'>Lorem ipsum dolor sit amet consectetur. Nulla vel nec morbi donec senectus pulvinar quis.</p>
-        <div className='flex gap-4 items-center'>
-            <div className='flex flex-col items-center'>
-                <p className='font-semibold text-32 bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>3000+</p>
-                <p className='font-medium text-sm'>Learners</p>
-            </div>
-            <img src={sugarCube} alt="" className='w-11 h-11' />
-            <div className='flex flex-col items-center'>
-                <p className='font-semibold text-32  bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>100+</p>
-                <p className='font-medium text-sm'>Courses</p>
-            </div>
-        </div>
+    <div className='bg-white mt-20 w-full h-full pb-10 gap-6 flex flex-col items-center pt-8'>
+        <p className='font-semibold text-2xl md:text-45'>Everything is Achievement</p>
+        <p className='text-gray-400 text-xs w-5/6 text-center md:text-16'>Lorem ipsum dolor sit amet consectetur. Nulla vel nec morbi donec senectus pulvinar quis.</p>
 
-        <div className='flex gap-4 items-center'>
-            <div className='flex flex-col items-center'>
-                <p className='font-semibold text-32  bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>25+</p>
-                <p className='font-medium text-sm'>Years of service</p>
+        <div className='flex flex-col items-center md:flex-row md:gap-10'>
+            <div className='flex gap-4 items-center'>
+                <div className='flex flex-col items-center'>
+                    <p className='font-semibold text-32 bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>3000+</p>
+                    <p className='font-medium text-sm'>Learners</p>
+                </div>
+                <img src={sugarCube} alt="" className='w-11 h-11' />
+                <div className='flex flex-col items-center'>
+                    <p className='font-semibold text-32  bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>100+</p>
+                    <p className='font-medium text-sm'>Courses</p>
+                </div>
             </div>
-            <img src={sugarCube} alt="" className='w-11 h-11' />
-            <div className='flex flex-col items-center'>
-                <p className='font-semibold text-32 bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>150+</p>
-                <p className='font-medium text-sm'>Countries connected</p>
+            <img src={sugarCube} alt="" className='hidden md:block' />
+
+            <div className='flex gap-4 items-center'>
+                <div className='flex flex-col items-center'>
+                    <p className='font-semibold text-32  bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>25+</p>
+                    <p className='font-medium text-sm'>Years of service</p>
+                </div>
+                <img src={sugarCube} alt="" className='w-11 h-11' />
+                <div className='flex flex-col items-center'>
+                    <p className='font-semibold text-32 bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>150+</p>
+                    <p className='font-medium text-sm'>Countries connected</p>
+                </div>
+                
             </div>
-            
         </div>
+        
 
     </div>
   )
--- src/Components/Discover.jsx
+++ src/Components/Discover.jsx
@@ -4,7 +4,14 @@ const Discover = () => {
   return (
     <section className='pt-28 flex flex-col items-center justify-center '>
         <button className='bg-white text-black py-3 px-4 font-semibold rounded-2xl mb-6 '>Trainings</button>
-        <h1 className='font-semibold text-2xl text-center mb-6 md:text-57 '><span className='bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>Discover</span> the world of <br /><span className='md:pt-5'>design</span></h1>
+        <h1 className='font-semibold flex flex-col gap-1 md:gap-5 text-2xl text-center mb-6 md:text-57 '>
+          <span className='flex gap-2'>
+            <span className='bg-gradient-to-r from-sinarpurple to-sinarblue inline-block text-transparent bg-clip-text'>Discover </span> 
+             <p>the world of </p>
+          </span>
+          
+          <span>design</span>
+        </h1>
         <div></div>
     </section>
   )
