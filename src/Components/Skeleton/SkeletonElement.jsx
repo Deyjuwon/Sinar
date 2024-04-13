289c4502fb775616123bec54ce8876145e208e86
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-04-13 23:37:45 +0100

    adding skeleton loader

--- src/Components/Skeleton/SkeletonElement.jsx
+++ src/Components/Skeleton/SkeletonElement.jsx
@@ -4,37 +4,30 @@ import Shimmer from './Shimmer'
 
 const SkeletonElement = () => {
   return (
-    <div className='bg-white  min-w-266  flex flex-col gap-5  px-4 rounded-xl pt-5 pb-5 mr-2  h-72'>
-        <img src='' alt=""  />
+    <div className='bg-white  min-w-266  flex flex-col gap-5  px-4 rounded-xl pt-5 pb-5 mr-2  h-fit'>
+      
+        <div className=' bg-gray-300 w-60 rounded-2xl h-36'></div>
         <img src='' alt="" className='absolute hidden' />
-        <div className='flex items-center justify-between gap-10'>
-            <div className='bg-dirty-white  min-w-fit  h-10 border shadow-lg  flex gap-2  items-center px-2  rounded-xl'>
-                <img src='' alt="" className=' h-2 w-2'   />
-                <p className=' text-sm font-medium'></p>
-            </div>
-            
-            <p className='font-semibold text-2xl'></p>
+        <div className='flex justify-between'>
+            <p className='h-7 w-20 bg-gray-300 rounded-lg'></p>
+            <p className='h-7 w-20 bg-gray-300 rounded-lg'></p>
         </div>
+        <div className='flex flex-col gap-2'>
+            <p className='h-3 w-full bg-gray-300'></p>
+            <p className='h-3 w-1/2 bg-gray-300'></p>
 
-        <p className=' font-semibold text-sm w-full'></p>
-
-        <div className='flex justify-between items-center'>
-            <img src='' alt="" className='h-11 w-11' />
-            <div className='flex gap-1 items-center'>
-                <img src='' alt="" className='w-4 h-4' />
-                <p></p>
-                </div>
-            <div className='flex gap-1 items-center'>
-                <img src='' alt="" className='w-4 h-4' />
-                <p></p>
-            </div>
-            <div className='flex gap-1 items-center'>
-                <img src='' alt="" className='w-4 h-4' />
-                <p>'</p>
-            </div>
-
-            
         </div>
+        <div className='flex items-center justify-between'>
+            <p className='rounded-full h-11 w-11 bg-gray-300' ></p>
+            <p className='h-3 w-8 bg-gray-300'></p>
+            <p className='h-3 w-8 bg-gray-300'></p>
+            <p className='h-3 w-8 bg-gray-300'></p>
+        </div>
+        
+
+        
+
+        
         <Shimmer />
     </div>
   )
