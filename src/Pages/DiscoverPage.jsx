5a7877fd36cc2229ecef68c705decbc2f8ee9e5e
Deyjuwon <temitayoadejuwon@gmail.com>
2024-04-18 04:53:21 +0100

    dicover page debugging

---
+++ src/Pages/DiscoverPage.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+import Header from '../Components/Header'
+import DiscoverHero from '../Components/DiscoverHero'
+import DiscoverCourse from '../Components/DiscoverCourse'
+import DiscoverCourses from '../Components/DiscoverCourses'
+
+const DiscoverPage = () => {
+
+  return (
+    <div className=''>
+        <Header />
+        <DiscoverHero />
+        <DiscoverCourse />
+        <DiscoverCourses />
+
+        
+
+        
+    </div>
+  )
+}
+
+export default DiscoverPage
