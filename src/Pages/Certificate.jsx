f21dfc4dbef85a6d6f1e424c845aff03056fdac9
Deyjuwon <temitayoadejuwon@gmail.com>
2024-04-30 16:51:02 +0100

    certificate page styling

--- src/Pages/Certificate.jsx
+++ src/Pages/Certificate.jsx
@@ -1,27 +1,54 @@
-import NotificationHeader from "../Components/NotificationHeader"
-import shield from '../assets/shield 1.png'
-
+import NotificationHeader from "../Components/NotificationHeader";
+import shield from '../assets/shield 1.png';
 
 const Certificate = () => {
   return (
-    <div className="px-4">
-        <NotificationHeader />
-        <div>
-            <p className="font-medium text-2xl pb-6'">Certificate</p>
-            <p>Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
-            <div className="flex gap-4 border border-gray-300 rounded-lg w-fit h-fit items-center p-4">
-                <div className="border border-gray-300 p-3 rounded-full ">
-                    <img className="object-cover w-9 h-9" src={shield} alt="" />
-                </div>
-                <div className="flex flex-col gap-2 ">
-                    <p>JavaScript</p>
-                    <p>Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
-                </div>
-
+    <div className="px-4 bg-white">
+      <NotificationHeader />
+      <div>
+        <p className="font-medium text-2xl pb-2">Certificates</p>
+        <p className="pb-8 text-sm  text-slate-500">Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
+        <div className="flex flex-col gap-3 ">
+          <div className="flex gap-4 border border-gray-300 rounded-lg w-full h-fit p-4  items-center">
+            <div className="rounded-full border border-gray-300 p-3  flex items-center justify-center w-1/5" >
+              <img src={shield} className="h-9 w-9" alt="" />
             </div>
+            <div className="flex flex-col gap-2 w-3/4">
+              <p className="text-btnBlue font-medium text-16">JavaScript</p>
+              <p className="text-xs  text-slate-500">Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 border border-gray-300 rounded-lg w-full h-fit p-4  items-center">
+            <div className="rounded-full border border-gray-300 p-3  flex items-center justify-center w-1/5" >
+              <img src={shield} className="h-9 w-9" alt="" />
+            </div>
+            <div className="flex flex-col gap-2 w-3/4">
+              <p className="text-btnBlue font-medium text-16">JavaScript</p>
+              <p className="text-xs  text-slate-500">Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 border border-gray-300 rounded-lg w-full h-fit p-4  items-center">
+            <div className="rounded-full border border-gray-300 p-3  flex items-center justify-center w-1/5" >
+              <img src={shield} className="h-9 w-9" alt="" />
+            </div>
+            <div className="flex flex-col gap-2 w-3/4">
+              <p className="text-btnBlue font-medium text-16">JavaScript</p>
+              <p className="text-xs  text-slate-500">Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 border border-gray-300 rounded-lg w-full h-fit p-4  items-center">
+            <div className="rounded-full border border-gray-300 p-3  flex items-center justify-center w-1/5" >
+              <img src={shield} className="h-9 w-9" alt="" />
+            </div>
+            <div className="flex flex-col gap-2 w-3/4">
+              <p className="text-btnBlue font-medium text-16">JavaScript</p>
+              <p className="text-xs  text-slate-500">Lorem ipsum dolor sit amet consectetur. In tristique facilisi a sit in quis vel.</p>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Certificate
+export default Certificate;
