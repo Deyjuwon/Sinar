c5dd968621cc500c677bd4cefe0b702f875b5c47
Deyjuwon <temitayoadejuwon@gmail.com>
2024-05-04 23:58:30 +0100

    code clean up

--- src/Pages/FeedbackPage.jsx
+++ src/Pages/FeedbackPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Feedback from '../Components/Feedback'
 
 const FeedbackPage = () => {
--- src/Pages/PaymentPanelPage.jsx
+++ src/Pages/PaymentPanelPage.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import CoursesPayment from '../Components/CoursesPayment'
-import ListCourses from '../Components/ListCourses'
+// import ListCourses from '../Components/ListCourses'
 
 const PaymentPanelPage = () => {
   return (
